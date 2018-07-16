b72d867e65192048b49381500f61281cb69f06d1
Kathryn <36640350+kattrinkr@users.noreply.github.com>
2018-07-16 16:54:48 +0300

    Part of the second task

---
+++ task2/1.js
@@ -0,0 +1,23 @@
+let Robot = function (name) {
+    this.name = name;
+}
+
+function add (op1, op2) {
+    this.name = this.name || "Human";
+    return this.name + ' can count to ' + (op1 + op2);
+}
+
+let voltron = new Robot("Voltron");
+
+console.log(add(0,1));
+console.log(add.call(voltron, 1, 2));
+console.log(add.apply(voltron, [20, 30]));
+console.log(add.bind(voltron)('drinking', 'beer'));
+
+function showName() {
+    setTimeout(function() {
+        console.log(name)
+    }, 0);
+};
+
+showName();
